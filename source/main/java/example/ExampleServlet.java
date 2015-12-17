587bccf6473761b503cc4b3f29072734ffbb684d
tanaka9230 <tanaka9230@users.noreply.github.com>
2015-12-17 21:01:39 +0900

    `setCharacterEncoding` added

--- source/main/java/example/ExampleServlet.java
+++ source/main/java/example/ExampleServlet.java
@@ -12,6 +12,7 @@ public class ExampleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
         response.getWriter().print(Sub.Text("Web"));
     }
 }
